fa3d497159d8863683de633757a2934744eb4f93
Muhammed Iqbal <iquzart@hotmail.com>
2020-05-28 19:09:36 +0400

    added html tag
    AB#3 Testing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return ('Hello from Flask App! hostname: {}\n'.format(socket.gethostname()))
+    return ('<H1> Hello from Flask App! hostname: {}\n </H1>'.format(socket.gethostname()))
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
