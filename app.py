6f3b1cf7d228ee4dee06dd252463cf0fda0f9f0f
iquzart <iquzart@hotmail.com>
2020-06-17 17:08:03 +0400

    Updated the app with bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import socket
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return ('<H1> Hello from Flask App! hostname: {}\n </H1>'.format(socket.gethostname()))
-    
+
+# Home
+@app.route('/') 
+def home():
+    hostname = socket.gethostname()
+    IP = (socket.gethostbyname(hostname))
+    print (IP)
+    return render_template('home.html', title='Home', hostname=hostname, IP=IP)  
+
+# Health
+@app.route('/health') 
+def health():
+    return('<H1>Working! </H1>')   
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+ 
+
+@app.errorhandler(404)  
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+    return render_template('404.html', title='404',)   
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
